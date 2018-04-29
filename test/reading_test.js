9b424d44cb1c33a44c6d0cc25a56acc37962b559
StephenDK <stephendklein08@gmail.com>
2018-04-29 16:56:37 -0700

    mongo queries

--- test/reading_test.js
+++ test/reading_test.js
@@ -2,14 +2,24 @@ const assert = require('assert');
 const User = require('../src/user');
 
 describe('Reading users out of the database', () => {
+    let joe;
 
-    beforeEach(() => {
-        joe = new User({ name: 'joe' });
+    beforeEach((done) => {
+        joe = new User({ name: 'Joe' });
         joe.save()
         .then(() => done());
     });
 
-    it('finds all users with the name of joe', () => {
-
+    it('finds all users with the name of joe', (done) => {
+        User.find({ name: 'Joe' })
+        .then((users) => {
+            console.log(users);
+            done();
+        })
     })
 })
+
+
+// models come with two finds
+// User.find(criteira) - find all users that match the give criteira. Returns an array;
+// User.findOne(criteira) - find the first user that matches the criteira. Returns a single record;
