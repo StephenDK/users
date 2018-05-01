6598d4c60d91fe907332943dfc55d1f733214ed1
StephenDK <stephendklein08@gmail.com>
2018-04-30 21:59:42 -0700

    update operators

--- src/user.js
+++ src/user.js
@@ -5,7 +5,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    name: String
+    name: String,
+    postCount: Number
 });
 
 // create user model
